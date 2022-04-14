25dae3bd87440746b3d70c8930572c0db6d6b60a
Gnomeek <gnomeek@outlook.com>
2022-04-14 18:06:35 +0800

    :wrench: update configs

--- configs/default.py
+++ configs/default.py
@@ -2,3 +2,5 @@ import os
 
 
 BASE_DIR = os.path.abspath(os.path.join(os.getcwd()))
+# change to false to use redis
+NO_REDIS = True
--- configs/dev.py
+++ configs/dev.py
@@ -2,3 +2,14 @@ from .default import *
 
 
 SERVER_TARGET = "tcp://0.0.0.0:1234"
+# change your db info
+DATABASES = {
+    "default": {
+        "ENGINE": "utils.db.engines.MysqlEngine",
+        "NAME": os.environ.get("DB_NAME", ""),
+        "USER": os.environ.get("DB_USER", ""),
+        "PASSWORD": os.environ.get("DB_PASSWD", ""),
+        "HOST": "localhost",
+        "PORT": "3306",
+    },
+}
