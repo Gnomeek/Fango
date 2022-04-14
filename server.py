130bfa98d7499d1cdff0a1e48d95574154dd2877
Gnomeek <gnomeek@outlook.com>
2022-04-14 18:02:10 +0800

    :sparkles: update main server entrance

--- server.py
+++ server.py
@@ -1,22 +1,25 @@
+import logging.config
+
 import flask
 
-from apps.pingpong import bp as demo_app
+from apps.pingpong import bp as pingpong_app
+from apps.store import bp as store_app
 from configs import ENV_PROFILE
 
-
 # from libs.hooks import after_request_func, before_request_func
-# from logging_config import config as logging_config
-# import logging
-#
-# # 初始化log
-# logging.config.dictConfig(logging_config)
-# logger = logging.getLogger(__name__)
+from logging_config import config as logging_config
+
+
+# 初始化log
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger(__name__)
 
 # 初始化server
 server = flask.Flask(__name__)
 
 # 注册蓝图
-server.register_blueprint(demo_app, url_prefix="/api/pingpong")
+server.register_blueprint(pingpong_app, url_prefix="/api/pingpong")
+server.register_blueprint(store_app, url_prefix="/api/store")
 
 # 注册请求钩子
 # server.before_request(before_request_func)
